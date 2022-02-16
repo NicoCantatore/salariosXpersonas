936613b4a8aac8e37488808cea5d0b5686eeda70
NicoCantatore <nicolas.cantatore@gmail.com>
2022-02-15 23:15:27 -0300

    v1

---
+++ salarioj.js
@@ -0,0 +1,62 @@
+const argentina = [];
+
+argentina.push({
+
+    nombre: "Nico",
+    salario: 70000,
+});
+
+argentina.push({
+
+    nombre: "Tolo",
+    salario: 80000,
+});
+
+argentina.push({
+
+    nombre: "Pelayo",
+    salario: 60000,
+});
+
+argentina.push({
+
+    nombre: "Conti",
+    salario: 100000,
+});
+
+argentina.push({
+
+    nombre: "Fer",
+    salario: 85000,
+});
+
+argentina.push({
+
+    nombre: "Laura",
+    salario: 40000,
+});
+
+
+argentina.push({
+
+    nombre: "Ruso",
+    salario: 20000,
+});
+
+argentina.push({
+
+    nombre: "Matias",
+    salario: 10000,
+});
+
+argentina.push({
+
+    nombre: "Pepe",
+    salario: 15000,
+});
+
+argentina.push({
+
+    nombre: "Practicante",
+    salario: 8000,
+});
